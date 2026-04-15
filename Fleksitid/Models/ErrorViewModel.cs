a15b9cead8b4b88b6050d2d49adc72934c0ed492
Thanh Nguyen <Thanhnguyen200107@gmail.com>
2026-04-15 21:10:05 +0200

    Add project files.

---
+++ Fleksitid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fleksitid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
