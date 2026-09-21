bec4ccdb2d49d69d3963b40fbf1d6f464d47b310
Thanh Nguyen <Thanhnguyen200107@gmail.com>
2026-09-21 18:59:20 +0200

    Legg til TidsregistreringController og Index-view for TimeEntry

---
+++ Fleksitid/Controllers/TidsregistreringController.cs
@@ -0,0 +1,34 @@
+using Fleksitid.Data;
+using Fleksitid.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fleksitid.Controllers
+{
+    [Authorize]
+    public class TidsregistreringController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public TidsregistreringController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+    {
+        _db = db;
+        _userManager = userManager;
+    }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var entries = await _db.TimeEntries
+                .Where(entries => entries.UserId == userId)
+                .OrderByDescending(entries => entries.Date)
+                .ToListAsync();
+
+            return View(entries);
+        }
+    }
+}
